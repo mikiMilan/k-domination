ff7f66716a7e457e124d8e06795da8d2d6815297
mickoMilan <predojevic.milan@yahoo.com>
2022-12-30 14:43:25 +0100

    corrected the code according to the PEP 8 standard

--- algorithms/cplex_k-d.py
+++ algorithms/cplex_k-d.py
@@ -1,44 +1,36 @@
 from time import time
-from networkx import \
-    DiGraph, \
-    Graph, \
-    gnp_random_graph as rand_graph, \
-    is_connected
-
-from pulp import * #LpProblem, lpSum, LpVariable
+from networkx import DiGraph, Graph, gnp_random_graph as rand_graph
+from pulp import LpProblem, lpSum, LpVariable, LpMinimize, LpBinary, PULP_CBC_CMD, LpStatus, value
 
 
 def ILP(graph: Graph or DiGraph, k: int, timelimit: float):
 
-	model = LpProblem("k-domination", LpMinimize)
-	y = LpVariable.dicts("x", list(graph.nodes),  0, 1, LpBinary)
-	#objective function: 
-	model += lpSum([ y[i] for i in list(graph.nodes) ] )
-	#constraints for each vertex i \in V:
-	for i in list(graph.nodes):
-		model += (k * y[i] + lpSum( [ y[j]  for j in list(g[i])  ]) >= k)
-		
-	## run model:
-	model.solve(PULP_CBC_CMD(maxSeconds = timelimit, msg=1, fracGap=0))		
-	## stats:
-	print("Status solving: ", LpStatus[ model.status ])
-	#print("Vars:")
-	#for v in model.variables():	
-	#	print(v.name, "===> ", v.varValue)
-        
-	print("Obj value: ", value(model.objective))
-	return value(model.objective) 
-	
+    model = LpProblem("k-domination", LpMinimize)
+    y = LpVariable.dicts("x", list(graph.nodes),  0, 1, LpBinary)
+
+    # objective function:
+    model += lpSum([y[i] for i in list(graph.nodes)])
+    # constraints for each vertex i in V:
+    for i in list(graph.nodes):
+        model += (k * y[i] + lpSum([y[j] for j in list(g[i])]) >= k)
+
+    # run model:
+    model.solve(PULP_CBC_CMD(maxSeconds=timelimit, msg=1, fracGap=0))
+    # stats:
+    print("Status solving: ", LpStatus[model.status])
+    # print("Vars:")
+    # for v in model.variables():
+    # 	print(v.name, "===> ", v.varValue)
+
+    print("Obj value: ", value(model.objective))
+    return value(model.objective)
+
 
 if __name__ == '__main__':
     size: int = 200
     timelimit: float = 30
     g = rand_graph(size, 0.5, seed=1)
-    # for i in range(10):
-    #     print(i, " - ", list(g[i]))
-    print("Connected: {}".format(is_connected(g)))
     curr = time()
     d1 = ILP(g, 3, timelimit)
     time_execute = time() - curr
-    print(time_execute, d1 )
-
+    print(time_execute, d1)
