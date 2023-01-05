2a680f0d16f2a5d1787220d4781eafd7f2cae35e
mickoMilan <predojevic.milan@yahoo.com>
2023-01-05 20:41:06 +0100

    improved BS

--- algorithms/beamSearchHeuristic.py
+++ algorithms/beamSearchHeuristic.py
@@ -1,96 +1,60 @@
 from time import time
-from networkx import \
-    DiGraph, \
-    Graph, \
-    gnp_random_graph as rand_graph, \
-    is_connected
-from unit import is_acceptable_solution, number_same_elements
-from random import shuffle
-from math import sqrt
+from networkx import DiGraph, Graph, gnp_random_graph as rand_graph
+from unit import is_acceptable_solution, objective_function
+import pickle
 
 
-def eqauls(l1, l2):
-
-    if len(l1) != len(l2):
-        return False
-
-    for i in range(0, len(l1)):
-        if l1[i] != l2[i]:
-            return False
-
-    return True
+def get_0(s: tuple) -> int:
+    return s[0]
 
 
-def remove_duplication(s):
+def remove_duplication(s: list):
     for i in range(len(s)-1, 0, -1):
         for j in range(0, i):
-            if eqauls(s[i], s[j]):
-                # print(s[i], s[j])
+            if s[i][0] == s[j][0] and s[i][1] == s[j][1]:
                 del s[i]
                 break
 
 
-def objective_function(s: list) -> int:
-    objective_sum: int = 0
-
-    for v in objective_function.graph.nodes:
-        if v not in s:
-            neighbors = set(g[v])
-            objective_sum += min(objective_function.k, number_same_elements(neighbors, s))
-
-    return objective_sum
-
-
-def beam_search_heuristic(graph: DiGraph or Graph, k: int) -> list:
-    # b = len(graph)/2
-    b = int(sqrt(len(graph)))
-    # print(b)
-
-    objective_function.graph = graph
-    objective_function.k = k
-
+def beam_search_heuristic(graph: DiGraph or Graph, k: int, b: int) -> list:
     d = []
-    s = [[]]
+    s = [(0, set())]
 
     while not d:
         s_prim = []
 
-        # print("Kreiram s prim")
-        rand_node = list(graph.nodes)
-        shuffle(rand_node)
         for parcial_s in s:
-            # last_element = parcial_s[-1] if len(parcial_s) > 0 else -1
-            for v in rand_node:
-                if v not in parcial_s:
-                    new_s = list(parcial_s)
-                    new_s.append(v)
-                    new_s.sort()
-                    s_prim.append(new_s)
+            for v in graph.nodes:
+                if v not in parcial_s[1]:
+                    new_s = set(parcial_s[1])
+                    new_s.add(v)
+                    s_prim.append((objective_function(list(new_s), graph, k), new_s))
 
-        remove_duplication(s)
-        print(s)
-        print("Sredjujem s")
         s = s_prim
-        # print(s)
-        s.sort(key=objective_function, reverse=True)
-        # print(s)
+        remove_duplication(s)
+        s.sort(key=get_0, reverse=True)
 
         while len(s) > b:
             s.pop()
         # print(s)
 
         for parcial_s in s:
-            if is_acceptable_solution(graph, parcial_s, k):
-                d = parcial_s
+            if is_acceptable_solution(graph, list(parcial_s[1]), k):
+                d = parcial_s[1]
 
     return d
 
 
 if __name__ == '__main__':
-    n = 200
-    g = rand_graph(n, 0.2, seed=1)
-    print("Connected: {}".format(is_connected(g)))
-    curr = time()
-    d1 = beam_search_heuristic(g, 1)
-    time_execute = time() - curr
-    print(time_execute, d1)
+    # n = 300
+    # g = rand_graph(n, 0.2, seed=2)
+    f = open("cities_small_instances/bath_pickle", "rb")
+    st_obj = pickle.load(f)
+    print(st_obj)
+
+
+    # curr = time()
+    # d1 = beam_search_heuristic(g, 1, b=4)
+    # time_execute = time() - curr
+    #
+    # print(len(d1), time_execute)
