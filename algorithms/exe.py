f8088aed2c487d3130fc0e50a218389811a777ba
mickoMilan <predojevic.milan@yahoo.com>
2023-01-05 14:20:17 +0100

    new instancess

--- algorithms/exe.py
+++ algorithms/exe.py
@@ -2,14 +2,15 @@ from time import time
 from networkx import gnp_random_graph as rand_graph
 from greedy import greedy
 from vns3 import vns
-from vns2 import vns as myvns
+from cplexKD import ILP
 from beamSearchHeuristic import beam_search_heuristic
 
 if __name__ == '__main__':
     alg: int = int(input("Izaberite algoritam (0-greedy, 1-vns, 2-myvns, 3-beamS): "))
     k: int = int(input("k-domination, k=: "))
     n: int = int(input("Broj cvorova: "))
-    # p: float = float(input("Gustina grana u procentima: "))
+    p: float = float(input("Gustina grana u procentima: "))
+    algs = [greedy, beam_search_heuristic, vns, ILP]
 
     for p in [0.2, 0.5, 0.8]:
         file_name = 'result/alg-'+str(alg)+' '+str(k)+'-dom num_ver-'+str(n)+' p-'+str(p)+'.txt'
@@ -18,7 +19,7 @@ if __name__ == '__main__':
             for i in range(15):
                 print("Graph ", i)
                 g = rand_graph(n, p, seed=i)
-                algs = [greedy, vns, myvns, beam_search_heuristic]
+
                 curr = time()
                 d = algs[alg](g, k)
                 time_execute = time() - curr
