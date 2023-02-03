c8140400a685ce74a68d6ce062c164dac8696781
mickoMilan <predojevic.milan@yahoo.com>
2023-02-03 10:47:04 +0100

    added grid search

--- algorithms/grid_search.py
+++ algorithms/grid_search.py
@@ -1,53 +1,73 @@
-import numpy as np
-from vns_estimator import VNS
-from sklearn.model_selection import GridSearchCV
+from vns import VNS
 from read_graph import read_graph
-from sklearn.metrics import accuracy_score, r2_score
-from sklearn.model_selection import train_test_split
-from model_estimator import TemplateEstimator
+from math import inf
 
 
-def GS(X, y, k, d_min, d_max, time_limit, iteration_max, prob, penalty):
-    param_grid = dict(k=np.array(k),
-                      d_min=np.array(d_min),
-                      d_max=np.array(d_max),
-                      time_limit=np.array(time_limit),
-                      iteration_max=np.array(iteration_max),
-                      prob=np.array(prob),
-                      penalty=np.array(penalty)
-                      )
+class grid_search():
+    def __init__(self, vns, param_space):
+        self.vns = vns
+        self.space = param_space
 
-    # create and fit a ridge regression model
-    model = VNS()
-    grid = GridSearchCV(
-        estimator=model,
-        param_grid=param_grid,
-        scoring=r2_score
-    )
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
-    grid.fit(X_train, y_train)
+    def run(self):
+        best_space = None
+        best_fitness = inf
+        # print("Inst:", self.vns.instance_name)
 
-    print(grid.best_score_)
-    print(grid.best_params_)
+        for curr_space in self.space:
+            self.vns.d_min = curr_space[0]
+            self.vns.d_max_init = curr_space[1]
+            self.vns.d_max = curr_space[1]
+            self.vns.prob = curr_space[2]
+            self.vns.penalty = curr_space[3]
 
-    # grid.score(X_test, y_test)
+            value, best_time, feasible = self.vns.run()
+
+            if not feasible:
+                best_space = None
+
+            fitness = len(value) + best_time/100000
+
+            if fitness < best_fitness:
+                best_space = curr_space
+                best_fitness = fitness
+                print("Inst:", self.vns.instance_name, "\t Len_Sol: ", len(value), "\t Space", best_space)
+
+        return best_space, best_fitness
 
 
 if __name__ == '__main__':
     instance_dir = 'cities_small_instances'
-    # instances = ['bath.txt', 'belfast.txt', 'brighton.txt', 'bristol.txt',
-    #              'cardiff.txt', 'coventry.txt', 'exeter.txt', 'glasgow.txt']
     instance = 'bath.txt'
+    time_limit = 3000
+    iteration_max = 1000
+
     graph_open = instance_dir + '/' + instance
     print("Reading graph!")
     g = read_graph(graph_open)
+    print("Creating process: ", graph_open)
 
-    X = [] # uzorak
-    n_samples = 10
-    for i in range(n_samples):
-        X.append([g, i, instance])
+    vns = VNS(instance, g, k=4, d_min=1, d_max_init=100, time_limit=time_limit, iteration_max=iteration_max, prob=0.5, penalty=0.01, rseed=12345)
 
-    y = [71] * n_samples  # opt k=2
+    # define grid parameters
+    d_min_space = (1, 10, 2)
+    d_max_init_space = (10, 101, 10)
+    prob_space = (0, 1, 0.2)
+    penalty_space = (0.001, 0.02, 0.004)
+    space = []
+    for dmn in range(d_min_space[0], d_min_space[1], d_min_space[2]):
+        for dmx in range(d_max_init_space[0], d_max_init_space[1], d_max_init_space[2]):
+            prob = prob_space[0]
+            while prob < prob_space[1]:
+                penalty = penalty_space[0]
+                while penalty < penalty_space[1]:
+                    space.append((dmn, dmx, prob, penalty))
+                    penalty += penalty_space[2]
+                prob += prob_space[2]
 
-    GS(X, y, k=[2], d_min=[1], d_max=[20, 15], time_limit=[1],
-       iteration_max=[3900], prob=[0.5], penalty=[0.01])
+    print("length grid space: ", len(space))
+    # print(space)
+
+    gr = grid_search(vns=vns, param_space=space)
+    # res = gr.run()
+
+    # print(res)
